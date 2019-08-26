c5e64f5800869e1b1a07d6f41eb905060bb67871
taehoonkim <taehoon.kim@bankwareglobal.com>
2019-08-26 18:00:23 +0900

    오늘 풀 문제

---
+++ alg/src/com/ikth/algs/baekjoon/ing/Q14499.java
@@ -0,0 +1,26 @@
+package com.ikth.algs.baekjoon.ing;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q14499 {
+
+	public static void main(String[] args) {
+		try(BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+				BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out))) {
+			
+			Q14499 m= new Q14499();
+			m.solution(br, bw);
+			bw.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void solution(BufferedReader br, BufferedWriter bw) throws NumberFormatException, IOException {
+		
+	}
+}
