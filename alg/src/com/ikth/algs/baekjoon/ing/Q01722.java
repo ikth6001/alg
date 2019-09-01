63a51e77d891460aa15301fa354e1adaee100613
ikthnism01 <ikthnism01@gmail.com>
2019-09-02 01:08:42 +0900

    음.. 일단 케이스1 풀이 완료..?? 제출 아직 안 함

--- alg/src/com/ikth/algs/baekjoon/ing/Q01722.java
+++ alg/src/com/ikth/algs/baekjoon/ing/Q01722.java
@@ -29,7 +29,99 @@ public class Q01722 {
 	 */
 	private void solution(BufferedReader br, BufferedWriter bw) throws NumberFormatException, IOException {
 		
+		int n= Integer.valueOf(br.readLine());
+		String[] row= br.readLine().split(" ");
 		
+		
+		switch(row[0]) {
+			case "1":
+				int[] nums= solution(n, Long.valueOf(row[1]));
+				for(int num : nums) {
+					bw.write(String.valueOf(num) + " ");
+				}
+				break;
+			case "2":
+				/**
+				 * TODO
+				 */
+				break;
+		}
+	}
+	
+	/**
+	 * case 1.
+	 * 
+	 * 1. boolean[1..n]을 생성하고 idx=1로 초기화 한다.
+	 * 1. (n-1)! 값을 구하여 nf 라고 한다.
+	 * 2. nf의 배수가 rank보다 클 때까지 idx++을 한다.
+	 * 3. idx를 수열의 숫자로 작성하고 boolean[idx]에 해당 숫자가 사용 되었음을 marking 한다.
+	 * 4. nf의 배수 - rank를 한다.
+	 * 5. nf의 배수 - rank가 0인 경우는 남은 수열의 자리는 최대 숫자가 되므로 boolean을 n부터 방문하며 사용 안 한 숫자를 하나하나 채워준다.
+	 * 6. nf의 배수 - rank가 0이 아닌 경우는 위 1~4를 반복한다.
+	 */
+	private int[] solution(int n, long rank) {
+		
+		boolean[] used= new boolean[n];
+		long[] facts= factorials(n);
+		int[] nums= new int[n];
+		int idx= 0;
+		
+		while(rank >= 0) {
+			
+			long fact= facts[n-idx-1];
+			int cnt= 0;
+			
+			if(rank == 0L) {
+				for(int i=idx; i<nums.length; i++) {
+					for(int j=used.length-1; j>=0; j--) {
+						if(!used[j]) {
+							nums[i]= j+1;
+							used[j]= true;
+							break;
+						}
+					}
+				}
+				break;
+			}
+			
+			long ofact= fact;
+			while(fact < rank) {
+				fact= fact + ofact;
+				cnt++;
+			}
+			
+			int num= getNum(used, cnt);
+			used[num-1]= true;
+			nums[idx++]= num;
+			rank= fact - rank;
+		}
+		
+		return nums;
+	}
+	
+	private int getNum(boolean[] used, int skip) {
+		int idx= 0;
+		while(true) {
+			
+			if(used[idx % used.length]) {
+				idx++;
+				continue;
+			} else if(skip-- == 0) {
+				return idx+1;
+			}
+			idx++;
+		}
+	}
+	
+	private long[] factorials(int n) {
+		long[] factorials= new long[n+1];
+		factorials[0]= 1L;
+		
+		for(int i=1; i<=n; i++) {
+			factorials[i]= factorials[i-1] * i;
+		}
+		
+		return factorials;
 	}
 	
 //	private void solution(BufferedReader br, BufferedWriter bw) throws NumberFormatException, IOException {
