b9a4eaccee9c93db47fa4d7c9568fa6c81fa11b1
ikthnism01 <ikthnism01@gmail.com>
2019-10-09 23:52:35 +0900

    10844번 어렵다..

---
+++ alg/src/com/ikth/algs/baekjoon/ing/Main.java
@@ -0,0 +1,36 @@
+package com.ikth.algs.baekjoon.ing;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * https://www.acmicpc.net/problem/10844
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		try(BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+				BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out))) {
+			
+			Main m= new Main();
+			m.solution(br, bw);
+			bw.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void solution(BufferedReader br, BufferedWriter bw) throws NumberFormatException, IOException {
+		
+		int n= Integer.parseInt(br.readLine());
+		int[] dp= new int[n];
+		dp[0]= 9;
+		
+		for(int i=1; i<n; i++) {
+			
+		}
+	}
+}
